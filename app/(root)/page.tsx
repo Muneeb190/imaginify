65c44545272d381adb31bb51b3d5b35725a5fdca
Muneeb <muneebkhan.mk2018@gmail.com>
2025-08-02 17:14:10 +0500

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import { UserButton } from '@clerk/nextjs'
+import { url } from 'inspector'
+import Image from 'next/image'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      home
+    </div>
+  )
+}
+
+export default Home
